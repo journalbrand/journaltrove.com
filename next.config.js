9ca1d9ef1e17b01f8b5b77b0cc3062efd281ec82
John W Benac <johnwbenac@Macmini.T-mobile.com>
2025-02-14 10:59:32 -0500

    Fix: Update Next.js config with correct asset paths

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Add basePath and assetPrefix for GitHub Pages
+  basePath: '',
+  assetPrefix: './',
 }
 
 module.exports = nextConfig 
